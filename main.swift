0131d8eac8f908684fea9d05cbbd62f7dfda1cc7
mnb3916 <912987977@qq.com>
2017-02-27 11:43:50 +0800

    Add files via upload

---
+++ main.swift
@@ -0,0 +1,27 @@
+//
+//  main.swift
+//  add100
+//
+//  Created by s20151104708 on 17/2/27.
+//  Copyright © 2017年 s20151104708. All rights reserved.
+//
+
+import Foundation
+
+var sum=0
+for i in 0...100
+{
+    sum = sum + i
+}
+print(sum)
+
+
+var j = 1
+var s = 0
+
+while j <= 100
+{
+    s += j
+    j += 1
+}
+print(s)
